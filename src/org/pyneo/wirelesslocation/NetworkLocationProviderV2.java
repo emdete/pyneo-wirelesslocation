cf97a6b7629158f4911484f4e6e4e95cc6715ae8
M. Dietrich <mdt@emdete.de>
2014-02-14 17:32:18 +0100

    integrate back cellapi

--- src/org/pyneo/wirelesslocation/NetworkLocationProviderV2.java
+++ src/org/pyneo/wirelesslocation/NetworkLocationProviderV2.java
@@ -11,7 +11,6 @@ import internal.com.android.location.provider.LocationProviderBase;
 import internal.com.android.location.provider.LocationRequestUnbundled;
 import internal.com.android.location.provider.ProviderPropertiesUnbundled;
 import internal.com.android.location.provider.ProviderRequestUnbundled;
-import java.lang.reflect.Method;
 
 @TargetApi(17)
 public class NetworkLocationProviderV2 extends LocationProviderBase implements NetworkLocationProvider {
@@ -105,43 +104,6 @@ public class NetworkLocationProviderV2 extends LocationProviderBase implements N
 		}
 		if (autoTime < 1000) {
 			autoTime = 1000;
-		}
-	}
-
-	static public Location dummy(double lat, double lon, double acc) {
-		Location location = new Location("network");
-		location.setProvider(NetworkLocationProvider.IDENTIFIER);
-		location.setLatitude(lat);
-		location.setLongitude(lon);
-		location.setAccuracy((float)acc);
-		location.setSpeed(0.0f);
-		location.setBearing(0.0f);
-		location.setAltitude(1.0);
-		//location.setElapsedRealtimeNanos(SystemClock.elapsedRealtime());
-		location.setTime(System.currentTimeMillis());
-		androidLocationLocationMakeComplete(location);
-		androidLocationLocationSetExtraLocation(location, new Location(location));
-		return location;
-	}
-
-	private static void androidLocationLocationMakeComplete(Location location) {
-		if (MainService.DEBUG) Log.d(TAG, "androidLocationLocationMakeComplete:");
-		try {
-			Class<?> clazz = Class.forName("android.location.Location");
-			Method makeComplete = clazz.getDeclaredMethod("makeComplete");
-			makeComplete.invoke(location);
-		} catch (Exception e) {
-			Log.w(TAG, "androidLocationLocationMakeComplete: " + e);
-		}
-	}
-
-	public static void androidLocationLocationSetExtraLocation(Location location, Location value) {
-		try {
-			Class<?> clazz = Class.forName("android.location.Location");
-			Method setExtraLocation = clazz.getDeclaredMethod("setExtraLocation", String.class, Location.class);
-			setExtraLocation.invoke(location, "noGPSLocation", value);
-		} catch (Exception e) {
-			Log.w("android.location.Location.setExtraLocation", e);
 		}
 	}
 }
